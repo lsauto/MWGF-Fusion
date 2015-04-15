41c86c503790b838a3b2943444913cbdbc52b5dd
lsauto <ls87auto@gmail.com>
2015-04-15 16:44:21 +0800

    using the imfilter to replace the conv2

--- WeightGradient.m
+++ WeightGradient.m
@@ -51,38 +51,14 @@ function [postMap, ss] = EigDecBlock(img, sigma)
     
     h = fspecial('gauss', [winSize winSize], sigma);
     
-%     win = floor(winSize/2);
     [hh, ww] = size(img); 
-%     imgPad = padarray(img, [win win], 'symmetric', 'both');
-    
-    postMap = zeros(hh, ww);
     ss = zeros(2, hh, ww);
-%     disp('The Eig Decompose:');
-%     for ii = 1:hh,
-%         for jj = 1:ww,
-%             patch = imgPad(ii:ii+winSize-1, jj:jj+winSize-1);
-%             dx = real(patch(:));
-%             dy = imag(patch(:));
-%             
-%             C = [sum(dx.*dx.*h(:)), sum(dx.*dy.*h(:));...
-%                  sum(dy.*dx.*h(:)), sum(dy.*dy.*h(:))];
-% 
-%             [V, D] = eig(C);
-%             
-%             postMap(ii, jj) = sqrt(D(2, 2)) * (V(1,2) + 1i*V(2, 2));   
-%             ss(:, ii, jj) = [abs(D(2, 2)) abs(D(1, 1))];
-%         end
-%         fprintf('.');
-%         if ~mod(ii, 30),
-%             fprintf('\n');
-%         end
-%     end
-%     fprintf('\n');
+
     dx = real(img);
     dy = imag(img);
-    dxx = conv2(dx.*dx, h, 'same');
-    dxy = conv2(dx.*dy, h, 'same');
-    dyy = conv2(dy.*dy, h, 'same');
+    dxx = imfilter(dx.*dx, h, 'symmetric');
+    dxy = imfilter(dx.*dy, h, 'symmetric');
+    dyy = imfilter(dy.*dy, h, 'symmetric');
     
     A = ones(size(img));
     B = -(dxx+dyy);
